1023455b38be1389c8042cddb0b10587f304b212
Mr-rakesh-rk <davinder02102001@gmail.com>
2024-07-18 12:46:14 +0530

    chnaes

--- src/Components/worldSection.jsx
+++ src/Components/worldSection.jsx
@@ -4,29 +4,33 @@ import Link from 'next/link';
 // import './css.scss';
 
 const countriesData = [
-  { name: 'Europe', countries: 7, imgSrc: '/path/to/europe.jpg' },
-  { name: 'North America', countries: 2, imgSrc: '/path/to/north-america.jpg' },
-  { name: 'Australia', countries: 6, imgSrc: '/path/to/australia.jpg' },
-  { name: 'Asia', countries: 4, imgSrc: '/path/to/asia.jpg' },
-  { name: 'South America', countries: 8, imgSrc: '/path/to/south-america.jpg' },
+  // G:\Node js Projects\my-travel-plan\src\app\assets\home_images\europe_img_1.png
+  { name: 'Europe', countries: 7, imgSrc: '/images/europe_img_1.png'  },
+  { name: 'North America', countries: 2, imgSrc: '/images/europe_img_1.png' },
+  { name: 'Australia', countries: 6, imgSrc: '/images/europe_img_1.png' },
+  { name: 'Asia', countries: 4, imgSrc: '/images/europe_img_1.png' },
+  { name: 'South America', countries: 8, imgSrc: '/images/europe_img_1.png' },
 ];
 
 const World_section = () => {
-  return (
+  return (  
     <div className="grid-container">
       {countriesData.map((country, index) => (
-        <Link href={`/${country.name.toLowerCase().replace(' ', '-')}`} key={index}>
+        <Link class="card_outer" href={`/${country.name.toLowerCase().replace(' ', '-')}`} key={index}>
           <div className="card">
             <div className="overlay">
               <div className="label">{country.countries}-Countries</div>
             </div>
-            <Image 
+
+            <Image
               src={country.imgSrc}
               alt={country.name}
-              width={300}
-              height={200}
+              style={{width:'100%', height:'100%'}}
+              width={1000}
+              height={300}
               className="image"
             />
+
             <div className="text">{country.name}</div>
           </div>
         </Link>
