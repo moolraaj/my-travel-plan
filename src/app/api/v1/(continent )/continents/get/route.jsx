4e555f445cf7bb77e54c2af5318dddfc64cb4322
moolraaj <116911577+moolraaj@users.noreply.github.com>
2024-07-27 14:10:59 +0530

    updated

--- src/app/api/v1/(continent )/continents/get/route.jsx
+++ src/app/api/v1/(continent )/continents/get/route.jsx
@@ -4,40 +4,24 @@ import { NextResponse } from "next/server";
 
 DbConnect();
 
-export async function GET(req, { params }) {
-    const { slug } = params;
+export async function GET() {
     try {
-        const continent = await continentModel.findOne({ slug }).populate({
-            path: 'all_countries',
-            populate: {
-                path: 'all_cities',
-                populate: {
-                    path: 'all_packages',
-                },
-            },
-        }).exec();
+        // Fetch and populate countries
+        const response = await continentModel.find().populate('all_countries').exec();
+        let totalResults=await continentModel.countDocuments()
 
-        if (continent) {
-            const result = continent.all_countries.map(country => ({
-                _id: country._id,
-                images: country.images,
-                title: country.title,
-                description: country.description,
-                slug: country.slug,
-                cities: country.all_cities.map(city => ({
-                    _id: city._id,
-                    city_name: city.title,
-                    city_packages_count: city.all_packages.length,
-                })),
-                total_cities: country.all_cities.length,
-            }));
+        let result=response.map((e)=>({
+            _id: e._id,
+            images: e.images,
+            title: e.title,
+            description: e.description,
+            slug: e.slug,
+            countriesCount:e.all_countries.length  
+        }))
 
-            return NextResponse.json({ success: true, result });
-        } else {
-            return NextResponse.json({ success: false, error: 'Continent not found' });
-        }
+        return NextResponse.json({ success: true,totalResults, result });
     } catch (error) {
         console.error('Error in GET handler:', error);
-        return NextResponse.json({ success: false, error: 'Failed to fetch continent and countries', message: error.message });
+        return NextResponse.json({ success: false, message: 'An error occurred', error: error.message });
     }
 }
