9dab585bd3c3eb3fea1e415637ddb25996f72057
moolraaj <116911577+moolraaj@users.noreply.github.com>
2024-07-27 15:06:16 +0530

    updated code

--- src/app/api/v1/(packages)/packages/get/route.jsx
+++ src/app/api/v1/(packages)/packages/get/route.jsx
@@ -1,7 +1,7 @@
 import { DbConnect } from "@/database/database";
 import PackagesModel from "@/model/packagesModel";
 import { NextResponse } from "next/server";
-DbConnect
+DbConnect()
 export async function GET(){
     let result=await PackagesModel.find()
     return NextResponse.json({success:true,result})
