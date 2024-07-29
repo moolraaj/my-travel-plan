ff013aa173d46dadbf6520669516f114d1897841
moolraaj <116911577+moolraaj@users.noreply.github.com>
2024-07-29 17:20:49 +0530

    updated code

--- src/app/api/v1/(cities)/cities/get/route.jsx
+++ src/app/api/v1/(cities)/cities/get/route.jsx
@@ -1,24 +1,36 @@
 import { DbConnect } from "@/database/database";
 import CitiesModel from "@/model/citiesModel";
 import { NextResponse } from "next/server";
+import { getPaginationParams } from "@/helpers/paginations";
+DbConnect();
 
-DbConnect()
-export async function GET(){
-    let data=await CitiesModel.find().populate('all_packages').exec()
-    let result=data.map((e)=>({
-        _id: e._id,      
-        images: e.images,
-        title: e.title,
-        description: e.description,
-        slug: e.slug,
-        packages:e.all_packages.map((pkg)=>({
-            _id: pkg._id,      
-            title: pkg.title,
-        })),
-        packagesCount:e.all_packages.length
-    }))
+export async function GET(req) {
+    let {page,limit,skip}=getPaginationParams(req)
+    try {
+        const data = await CitiesModel.find()
+            .populate('all_packages')
+            .skip(skip)
+            .limit(limit)
+            .exec();
 
-    console.log(data)
-    let totalResults=await CitiesModel.countDocuments()
-    return NextResponse.json({success:true,totalResults,result})
+        const result = data.map(e => ({
+            _id: e._id,
+            images: e.images,
+            title: e.title,
+            description: e.description,
+            slug: e.slug,
+            packages: e.all_packages.map(pkg => ({
+                _id: pkg._id,
+                title: pkg.title,
+            })),
+            packagesCount: e.all_packages.length
+        }));
+
+        const totalResults = await CitiesModel.countDocuments();
+
+        return NextResponse.json({ success: true, totalResults,result,page,limit});
+    } catch (error) {
+        console.error('Error in GET handler:', error);
+        return NextResponse.json({ success: false, message: 'An error occurred', error: error.message });
+    }
 }
--- src/app/api/v1/(packages)/packages/get/route.jsx
+++ src/app/api/v1/(packages)/packages/get/route.jsx
@@ -2,7 +2,19 @@ import { DbConnect } from "@/database/database";
 import PackagesModel from "@/model/packagesModel";
 import { NextResponse } from "next/server";
 DbConnect()
-export async function GET(){
-    let result=await PackagesModel.find()
-    return NextResponse.json({success:true,result})
+export async function GET() {
+    try {
+        
+        let data = await PackagesModel.find()
+        let result = data.map((e) => ({
+            _id: e._id,
+            images: e.images,
+            title: e.title,
+        description: e.description,
+            slug: e.slug,
+        }))
+        return NextResponse.json({ success: true, result })
+    } catch (error) {
+        return NextResponse.json({ success: false, message:'internal server error' }) 
+    }
 }
