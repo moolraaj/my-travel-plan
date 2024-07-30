ad43d959676cb80065c6a0c0d097b4d670505d59
neosharma995 <neosharma995@gmail.com>
2024-07-30 15:29:36 +0530

    changes in contact us

---
+++ src/app/(contactus)/contact-us/page.jsx
@@ -0,0 +1,43 @@
+import AddressDetail from '../components/address_detail';
+import Layout from '@/app/_common/layout/layout';
+import ContactForm from '../Components/contactForm';
+import Map from '../Components/map';
+import Image from 'next/image';
+import Link from 'next/link';
+export default function contact() {
+  return (
+<Layout>
+    <div className='main_slider contact_left_vector'>
+      <div className='outer_sectioncontact'>
+        <div className='breadcrumbs_section contact_breadcrumbs' style={{ backgroundImage: 'url(/images/contact.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+          <div className='inner-w-container'>
+            <div className='breadcrumbs_inner_section'>
+              <div className='breadcrumb-heading'>
+                <h2>Contact Us</h2>
+              </div>
+              <div className='breadcrumb'>
+                <Link href="/">
+                  Home
+                </Link>
+                <a> / Contact Us</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='explorations-container'>
+          <AddressDetail />
+        </div>
+        <div className='form_container' style={{ backgroundImage: 'url(/images/tourist.png)', backgroundSize: '568px', backgroundPosition: 'right bottom', backgroundRepeat: 'no-repeat' }}>
+          <div className='explorations-container'>
+            <ContactForm />
+          </div>
+        </div>
+        <div className='explorations-container'>
+          <Map />
+        </div>
+      </div>
+
+    </div>
+    </Layout>
+  );
+}
