10434171988db81a7a8000d0c22bd0d985416763
moolraaj <116911577+moolraaj@users.noreply.github.com>
2024-08-01 14:24:39 +0530

    updated code

--- src/app/api/v1/(users)/sendquery/queries/get/route.jsx
+++ src/app/api/v1/(users)/sendquery/queries/get/route.jsx
@@ -3,5 +3,6 @@ import { NextResponse } from "next/server";
 
 export async function GET(){
     let result=await ContactModel.find()
-    return NextResponse.json({status:200,result})
+    let totalResults=await ContactModel.countDocuments()
+    return NextResponse.json({status:200,totalResults,result})
 }
