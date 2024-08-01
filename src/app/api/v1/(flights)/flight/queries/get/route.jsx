f967ad5cac1c84f50d355891a259784b6ddf276f
moolraaj <116911577+moolraaj@users.noreply.github.com>
2024-08-01 14:24:39 +0530

    updated code

--- src/app/api/v1/(flights)/flight/queries/get/route.jsx
+++ src/app/api/v1/(flights)/flight/queries/get/route.jsx
@@ -4,5 +4,6 @@ import { NextResponse } from "next/server";
 
 export async function GET(){
     let result=await FlightModel.find()
-    return NextResponse.json({status:200,result})
+    let totalResults=await FlightModel.countDocuments()
+    return NextResponse.json({status:200,totalResults,result})
 }
