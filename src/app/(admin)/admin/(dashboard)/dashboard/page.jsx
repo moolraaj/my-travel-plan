a26e44acf2dea5d2d79f8fd913c2ba88e788d44e
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-08-01 14:16:17 +0530

    changes

--- src/app/(admin)/admin/(dashboard)/dashboard/page.jsx
+++ src/app/(admin)/admin/(dashboard)/dashboard/page.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Overview from '../components/Overview'
 import Stats from '../components/Stats'
 import RecentBookings from '../components/RecentBookings'
+import DataPercentage from '../components/DataPercentage'
 
 function page() {
   return (
     <div className="dashboard-page">
       <Overview />
       <Stats />
+      <DataPercentage/>
       <RecentBookings />
     </div>
   )
