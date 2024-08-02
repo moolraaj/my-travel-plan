1643205aa03972c625013fcaef2a8ac8b309f53e
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-08-02 13:38:08 +0530

    changes

--- src/utils/apis/apis.jsx
+++ src/utils/apis/apis.jsx
@@ -1,11 +1,49 @@
+// /utils/apis/apis.jsx
+
 export const GETALLAPISEXPORTS=()=>{
 
-    const getdata=async()=>{
+    const URI = process.env.WEB_URI
 
+    const getContinents=async()=>{
+        let data = await fetch(`${URI}/api/v1/continents/get`,{ cache: 'no-store' });
+        let res = await data.json();
+        return res;
     }
 
+    const getCountries=async()=>{
+        let data = await fetch(`${URI}/api/v1/countries/get`,{ cache: 'no-store' });
+        let res = await data.json();
+        return res;
+    }
+
+    const getCities=async()=>{
+        let data = await fetch(`${URI}/api/v1/cities/get`,{ cache: 'no-store' });
+        let res = await data.json();
+        return res;
+    }
+    const getPackages=async()=>{
+        let data = await fetch(`${URI}/api/v1/packages/get`,{ cache: 'no-store' });
+        let res = await data.json();
+        return res;
+    }
+    
+    const getContacts=async()=>{
+        let data = await fetch(`${URI}/api/v1/sendquery/queries/get`,{ cache: 'no-store' });
+        let res = await data.json();
+        return res;
+    }
+    const getBookings=async()=>{
+        let data = await fetch(`${URI}/api/v1/flight/queries/get`,{ cache: 'no-store' });
+        let res = await data.json();
+        return res;
+    }
     return{
-        getdata
+        getContinents,
+        getCountries,
+        getCities,
+        getPackages,
+        getContacts,
+        getBookings
     }
 
 }
