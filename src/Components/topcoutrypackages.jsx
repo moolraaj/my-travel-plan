5d3c1511c81bcbba9eabc7c58472de05995360bd
moolraaj <116911577+moolraaj@users.noreply.github.com>
2024-08-05 14:45:05 +0530

    update package apis

--- src/Components/topcoutrypackages.jsx
+++ src/Components/topcoutrypackages.jsx
@@ -1,51 +1,76 @@
+'use client'
 import Image from 'next/image';
 import Link from 'next/link';
 import trending from '../app/assets/home_images/trending.png';
 import ribbon from '../app/assets/home_images/ribbon.png';
 import fishbg from '../app/assets/home_images/fish-bg.png';
+import { useEffect, useState } from 'react';
+import { EXPORT_ALL_APIS } from '@/utils/apis/api';
 
 
-const countriesData = [
-  { name: 'Austria', countries: 5, imgSrc: '/images/austria.png' },
-  { name: 'Belgium', countries: 3, imgSrc: '/images/belgium.png' },
-  { name: 'Czech Republic', countries: 7, imgSrc: '/images/czechrepublic.png' },
-  { name: 'Denmark', countries: 2, imgSrc: '/images/denmark.png' },
-  { name: 'Estonia', countries: 1, imgSrc: '/images/estonia.png' },
-  { name: 'Finland', countries: 5, imgSrc: '/images/finland.png' },
-  { name: 'France', countries: 6, imgSrc: '/images/france.png' },
-  { name: 'Germany', countries: 3, imgSrc: '/images/germany.png' },
-];
+ 
 
 const Destinations = () => {
+  let api = EXPORT_ALL_APIS()
+  let [data, setData] = useState([])
+  let fetchAllCountries = async () => {
+    let resp = await api.loadAllCountries()
+    setData(resp)
+  }
+
+  useEffect(() => {
+    fetchAllCountries()
+  }, [])
+
+  let result = data ? data.result : [];
+
+  let reversedCountries = Array.isArray(result) ? [...result].reverse() : [];
+
   return (
-    <div className='top-destination' style={{ backgroundImage: `url(${fishbg.src})`}}>
-    <div className="topdestination container inner-w-container">
-      <h2 className='same_heading'>Top Destination By Our Travel Experts</h2>
-      <p>Unlimited Choices | Best Prices | Happy Memories | Hot Deals</p>
-      <div className="destinations expert-travel">
-        {countriesData.map((country, index) => (
-          <div key={index} className="destination">
-            <img className= "expert-image" src={country.imgSrc} alt={country.name}  />            
-            <span
+    <div className='top-destination' style={{ backgroundImage: `url(${fishbg.src})` }}>
+      <div className="topdestination container inner-w-container">
+        <h2 className='same_heading'>Top Destination By Our Travel Experts</h2>
+        <p>Unlimited Choices | Best Prices | Happy Memories | Hot Deals</p>
+        <div className="destinations expert-travel">
+          {reversedCountries===null||reversedCountries===undefined?('no result found'):(reversedCountries.slice(0,8).map((country, index) => (
+            <Link href={`/${country.slug.toLowerCase().replace(' ', '-')}`}>
+
+            <div key={index} className="destination">
+             
+              {country.images === null || country.images === undefined ? ('no result found') : country.images.map((e) => {
+                return <Image
+                  key={e._id}
+                  src={`/uploads/${e.name}`}
+                  alt={country.name}
+                  style={{ width: '100%', height: '100%' }}
+                  width={1000}
+                  height={300}
+                  className="image"
+                />
+              })}
+
+              <span
                 style={{
                   backgroundImage: `url(${ribbon.src})`,
                   backgroundSize: 'cover',
                   backgroundRepeat: 'no-repeat',
-                                 }}
+                }}
                 className="trending"
-              ><img src={trending.src}></img>
-              TREADING
-               </span>
-            <div className="info">
-              <h3>{country.name}</h3>
-              <p>{country.countries} Packages</p>
-              
+              >
+                <img src={trending.src}></img>
+                TREADING
+              </span>
+              <div className="info">
+                <h3>{country.title}</h3>
+                <p>{country.countries} Packages</p>
+
+              </div>
             </div>
-          </div>
-        ))}
+            </Link>
+          )))}
+        </div>
+
       </div>
-     
-    </div>
     </div>
   );
 };
---
+++ src/utils/apis/api.jsx
@@ -0,0 +1,45 @@
+import { handelAsyncErrors } from "@/helpers/asyncErrors";
+
+export const EXPORT_ALL_APIS = () => {
+    const loadAllContinents = async () => {
+        return await handelAsyncErrors(async () => {
+            let resp = await fetch(`${process.env.NEXT_PUBLIC_HOST_URL}/api/v1/continents/get`);
+            let data = await resp.json();
+            return data;
+        });
+    };
+
+
+    const loadAllCountries = async () => {
+        return await handelAsyncErrors(async () => {
+            let resp = await fetch(`${process.env.NEXT_PUBLIC_HOST_URL}/api/v1/countries/get`);
+            let data = await resp.json();
+            return data;
+        });
+    };
+    const loadAllPackages = async () => {
+        return await handelAsyncErrors(async () => {
+            let resp = await fetch(`${process.env.NEXT_PUBLIC_HOST_URL}/api/v1/packages/get`);
+            let data = await resp.json();
+            return data;
+        });
+    };
+    const loadAllBlogs = async () => {
+        return await handelAsyncErrors(async () => {
+            let resp = await fetch(`${process.env.NEXT_PUBLIC_HOST_URL}/api/v1/blogs/get`);
+            let data = await resp.json();
+            return data;
+        });
+    };
+
+    
+    return {
+        loadAllContinents,
+        loadAllCountries,
+        loadAllPackages,
+        loadAllBlogs
+    };
+};
+
+
+export const PER_PAGE_LIMIT=10
