5d71a6fbd69e3651ae4e4203bea2a49b6b867d29
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-08-07 16:51:04 +0530

    changes

--- src/app/(admin)/_common/layout/adminLayout.jsx
+++ src/app/(admin)/_common/layout/adminLayout.jsx
@@ -1,21 +1,33 @@
 // /app/(admin)/_common/layout/adminLayout.jsx
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import '../../Admin.css';
 import AdminNavbar from '../header/components/navbar';
 import Adminfooter from '../footer/components/footer';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Sidebar from '../sidebar/components/sidebar';
 
 function AdminLayout({children}) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
   return (
-    <div>
+    <div className='admin_outer_layout'>
+      <div className='sidebar_contexts'>
+        <div className={`wrapper ${sidebarOpen ? 'sidebar-open' : 'sidebar-closed'}`}>
+      <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
+      </div>
+      </div>
+      <div className='admin_contexts'>
       <AdminNavbar/>
       <div className='admin_layout'>
         <ToastContainer/>
       {children}
       </div>
       <Adminfooter/>
+      </div>
     </div>
   );
 }
