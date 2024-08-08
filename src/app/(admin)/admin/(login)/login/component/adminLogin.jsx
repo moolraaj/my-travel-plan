02e14daef6acd7597e2be06c86fbaf20476cde64
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-08-08 11:52:55 +0530

    changes

--- src/app/(admin)/admin/(login)/login/component/adminLogin.jsx
+++ src/app/(admin)/admin/(login)/login/component/adminLogin.jsx
@@ -2,7 +2,9 @@
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 import { signIn } from 'next-auth/react';
-import logo from '@/app/assets/home_images/logo.png'
+import logo from '@/app/assets/home_images/logo.png';
+import LoadingOverlay from '../../../(dashboard)/components/LoadingOverlay';
+import { handelAsyncErrors } from '@/helpers/asyncErrors';
 
 function AdminLoginPage() {
     let router = useRouter();
@@ -11,13 +13,25 @@ function AdminLoginPage() {
         password: ''
     });
     let [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const onChangeHandler = (e) => {
         setData({ ...data, [e.target.name]: e.target.value });
+        setError({ ...error, [e.target.name]: '' });
     };
 
     const logedInAdmin = async () => {
-        try {
+        if (!data.email || !data.password) {
+            setError({
+                email: !data.email ? 'Email is required' : '',
+                password: !data.password ? 'Password is required' : '',
+                general: ''
+            });
+            return;
+        }
+
+        setLoading(true);
+        return handelAsyncErrors (async()=>{
             let resp = await fetch('/api/v1/admin/login', {
                 method: 'POST',
                 headers: {
@@ -40,21 +54,24 @@ function AdminLoginPage() {
             } else {
                 setError(result.message || 'An error occurred');
             }
-        } catch (err) {
-            setError('An error occurred');
-        }
+        })
+        
+            
+        
     };
 
     return (
         <div className="admin_login_container">
+             {loading && <LoadingOverlay />}
             <div className="admin_login_logo">
                 <img src={logo.src} />
             </div>
             <h1>Admin Login</h1>
             <input type="email" name="email" placeholder="Email" value={data.email} onChange={onChangeHandler} />
+            {error.email && <p className="admin_login_error">{error.email}</p>}
             <input type="password" name="password" placeholder="Password" value={data.password} onChange={onChangeHandler} />
+            {error.password && <p className="admin_login_error">{error.password}</p>}
             <button onClick={logedInAdmin}>Login</button>
-            {error && <p className="admin_login_error">{error}</p>}
         </div>
     );
 }
