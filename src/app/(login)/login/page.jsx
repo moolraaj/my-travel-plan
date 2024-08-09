c12dfb36c166a737dee33172741f65e32f4a5bfe
moolraaj <116911577+moolraaj@users.noreply.github.com>
2024-08-09 12:03:56 +0530

    updated code

--- src/app/(login)/login/page.jsx
+++ src/app/(login)/login/page.jsx
@@ -1,12 +1,14 @@
 'use client'
-import React from 'react'
+import React, { Suspense } from 'react'
 import Login from '../Components/login'
 import loginbg from '../../../../public/images/login-bg.png'
 
 function page() {
   return (
     <div className='popup-bg' style={{ backgroundImage: `url(${loginbg.src})` }}>
+      <Suspense fallback={<div>Loading...</div>}>
       <Login/>
+    </Suspense>
     </div>
   )
 }
