7c13bd501e00acf90443d961d02014f9f5811dbc
moolraaj <116911577+moolraaj@users.noreply.github.com>
2024-08-10 14:01:29 +0530

    updated code

--- src/app/page.js
+++ src/app/page.js
@@ -64,7 +64,7 @@ export default  function Home() {
     
     <>
       <Layout>
-        <h1></h1>
+    
       
         <Homepage continent={continent} loading={loading} country={country} city={city} packages={packages} blogs={blogs}/>
 
