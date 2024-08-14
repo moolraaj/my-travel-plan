daee48750c1409ec0a0796a45dc1429376b3b5d5
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-08-14 17:18:47 +0530

    changes

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,25 +1,22 @@
 'use client';
 
- 
-import { Blocks } from 'react-loader-spinner';
+
+import Image from 'next/image';
+import loader from '../../public/uploads/airplane.gif';
 
 function Loading() {
-   
+
 
   return (
     <>
       <div className="loading_wrapper">
-        
-          <Blocks
-            height="180"
-            width="180"
-            color="#000"
-            ariaLabel="blocks-loading"
-            wrapperStyle={{}}
-            wrapperClass="blocks-wrapper"
-            visible={true}
-          />
-        
+
+        <Image src={loader.src}
+          width={180}
+          height={180}
+
+        />
+
       </div>
     </>
   );
