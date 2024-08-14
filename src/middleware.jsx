37a98203985f1de9796b178d9cd511b7f2d037ca
moolraaj <116911577+moolraaj@users.noreply.github.com>
2024-08-14 19:28:44 +0530

    updated

--- src/middleware.jsx
+++ src/middleware.jsx
@@ -17,46 +17,30 @@ export default async function middleware(request) {
   const adminPrivateRoutes = ['/admin/dashboard'];
   const userPrivateRoutes = ['/dashboard'];
 
-  // Get the country code from the request's geolocation data
-  const country = request.geo?.country || '';
-
-  // Country-based redirection
-  if (country === 'MY') {
-    return NextResponse.redirect('https://mieland.com.my');
+  // Redirect admin users who are trying to access user public routes
+  if (token && user.role === 'admin' && userPublicRoutes.includes(pathname)) {
+    return NextResponse.redirect(new URL('/admin/dashboard', request.url));
   }
 
-  if (country === 'US') {
-    return NextResponse.redirect('https://www.eligocs.com');
+  // Redirect regular users who are trying to access user public routes
+  if (token && user.role === 'user' && userPublicRoutes.includes(pathname)) {
+    return NextResponse.redirect(new URL(request.headers.get('referer') || '/dashboard', request.url));
+
   }
 
-  // Role-based redirection logic
-  if (token) {
-    if (user.role === 'admin') {
-      // Redirect admin users who are trying to access user public routes
-      if (userPublicRoutes.includes(pathname)) {
-        return NextResponse.redirect(new URL('/admin/dashboard', request.url));
-      }
-    } else if (user.role === 'user') {
-      // Redirect regular users who are trying to access user public routes
-      if (userPublicRoutes.includes(pathname)) {
-        return NextResponse.redirect(new URL(request.headers.get('referer') || '/dashboard', request.url));
-      }
+  // Redirect users with role 'user' who are trying to access admin routes, except '/admin/login'
+  if (token && user.role === 'user' && pathname.startsWith('/admin') && !adminPublicRoutes.includes(pathname)) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
 
-      // Redirect users with role 'user' who are trying to access admin routes, except '/admin/login'
-      if (pathname.startsWith('/admin') && !adminPublicRoutes.includes(pathname)) {
-        return NextResponse.redirect(new URL('/login', request.url));
-      }
-    }
-  } else {
-    // Redirect unauthenticated users trying to access private user routes
-    if (userPrivateRoutes.includes(pathname)) {
-      return NextResponse.redirect(new URL('/login', request.url));
-    }
+  // Redirect unauthenticated users trying to access private user routes
+  if (!token && userPrivateRoutes.includes(pathname)) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
 
-    // Redirect unauthenticated users trying to access admin routes
-    if (pathname.startsWith('/admin') && !adminPublicRoutes.includes(pathname)) {
-      return NextResponse.redirect(new URL('/admin/login', request.url));
-    }
+  // Redirect unauthenticated users trying to access admin routes
+  if (!token && pathname.startsWith('/admin') && !adminPublicRoutes.includes(pathname)) {
+    return NextResponse.redirect(new URL('/admin/login', request.url));
   }
 
   // Proceed with request if no redirects
