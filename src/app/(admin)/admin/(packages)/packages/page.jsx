97efdc64dca1573a130a891d1c5eacec42095642
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-08-16 15:14:20 +0530

    changes

--- src/app/(admin)/admin/(packages)/packages/page.jsx
+++ src/app/(admin)/admin/(packages)/packages/page.jsx
@@ -5,7 +5,7 @@
 import React, { useEffect, useState } from 'react';
 import { FaEye, FaEdit, FaTrashAlt, FaPlus } from 'react-icons/fa';
 import { useRouter } from 'next/navigation';
-import {  toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import ModalWrapper from '@/app/(admin)/_common/modal/modal';
 import Breadcrumb from '@/app/(admin)/_common/Breadcrumb';
 
@@ -46,24 +46,24 @@ function Packages() {
   };
 
   const handleConfirm = async () => {
-    
-      try {
-        const response = await fetch(`/api/v1/package/delete/${deleteItem}`, { method: 'DELETE' });
-        const data = await response.json();
-        if (data.success) {
-          fetchPackages();
-          toast.success(data.message || 'Package deleted successfully.');
-          setIsOpen(false)
-        } else {
-          toast.error(data.message ||'Failed to delete package.');
-        }
-      } catch (error) {
-        toast.error('Failed to delete package, please try again.');
+
+    try {
+      const response = await fetch(`/api/v1/package/delete/${deleteItem}`, { method: 'DELETE' });
+      const data = await response.json();
+      if (data.success) {
+        fetchPackages();
+        toast.success(data.message || 'Package deleted successfully.');
+        setIsOpen(false)
+      } else {
+        toast.error(data.message || 'Failed to delete package.');
       }
-    
+    } catch (error) {
+      toast.error('Failed to delete package, please try again.');
+    }
+
   };
 
-  const handleDelete=(id)=>{
+  const handleDelete = (id) => {
     setIsOpen(true)
     setDeleteItem(id)
   }
@@ -85,16 +85,16 @@ function Packages() {
   return (
     <div className="packages">
       <ModalWrapper
-      isOpen={isOpen}
-      onClose={()=>setIsOpen(false)}
-      onConfirm={handleConfirm}
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        onConfirm={handleConfirm}
       />
       <div className="package_header">
-      <Breadcrumb path="/admin/packages" />
-      <div className="floating-plus" onClick={handleAddClick}>
-        <FaPlus />
-        <div className="add_tooltip">Add Package</div>
-      </div>
+        <Breadcrumb path="/admin/packages" />
+        <div className="floating-plus" onClick={handleAddClick}>
+          <FaPlus />
+          <div className="add_tooltip">Add Package</div>
+        </div>
       </div>
       <div className="packages-table-container">
         <table className="packages-table">
@@ -103,32 +103,42 @@ function Packages() {
               <th>Image</th>
               <th>ID</th>
               <th>Title</th>
-              <th>Description</th>
+              <th>Under Continent</th>
+              <th>Under Country</th>
+              <th>Under City</th>
               <th>Actions</th>
             </tr>
           </thead>
           <tbody>
             {loading ? (
               <tr>
-                <td colSpan="5" className="loading">Loading...</td>
+                <td colSpan="7" className="loading">Loading...</td>
               </tr>
             ) : packages.length === 0 ? (
               <tr>
-                <td colSpan="6" className="no-data">No Packages Available</td>
+                <td colSpan="7" className="no-data">No Packages Available</td>
               </tr>
-            ): (
+            ) : (
               packages.map(pkg => (
                 <tr key={pkg._id}>
                   <td data-label="Image">
-                    <img 
-                      src={`/uploads/${pkg.images[0].name}`} 
-                      alt={pkg.title} 
-                      className="package-image" 
+                    <img
+                      src={`/uploads/${pkg.images?.[0]?.name}`}
+                      alt={pkg.title}
+                      className="package-image"
                     />
                   </td>
                   <td data-label="ID">{pkg._id}</td>
                   <td data-label="Title">{pkg.title}</td>
-                  <td data-label="Description">{pkg.description}</td>
+                  <td data-label="Under Continent">
+                    {pkg.package_under_continent ? pkg.package_under_continent.title : "N/A"}
+                  </td>
+                  <td data-label="Under Country">
+                    {pkg.package_under_country ? pkg.package_under_country.title : "N/A"}
+                  </td>
+                  <td data-label="Under City">
+                    {pkg.package_under_city ? pkg.package_under_city.title : "N/A"}
+                  </td>
                   <td data-label="Actions">
                     <span className="actions">
                       <FaEye className="action-icon view" title="View" onClick={() => handlePreview(pkg._id)} />
@@ -143,14 +153,14 @@ function Packages() {
         </table>
       </div>
       <div className="pagination">
-        <button 
+        <button
           onClick={() => handlePageChange(1)}
           disabled={currentPage === 1}
           className="pagination-button"
         >
           {'<<'}
         </button>
-        <button 
+        <button
           onClick={() => handlePageChange(currentPage - 1)}
           disabled={currentPage === 1}
           className="pagination-button"
@@ -158,14 +168,14 @@ function Packages() {
           {'<'}
         </button>
         <span className="pagination-info">Page {currentPage} of {totalPages}</span>
-        <button 
+        <button
           onClick={() => handlePageChange(currentPage + 1)}
           disabled={currentPage === totalPages}
           className="pagination-button"
         >
           {'>'}
         </button>
-        <button 
+        <button
           onClick={() => handlePageChange(totalPages)}
           disabled={currentPage === totalPages}
           className="pagination-button"
@@ -173,7 +183,7 @@ function Packages() {
           {'>>'}
         </button>
       </div>
-      
+
     </div>
   );
 }
