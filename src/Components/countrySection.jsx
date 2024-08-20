fca028d198b3bdce194dc4db41553c53ba595bdb
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-08-20 19:46:38 +0530

    changes

--- src/Components/countrySection.jsx
+++ src/Components/countrySection.jsx
@@ -1,62 +1,64 @@
 'use client';
 
- 
 import Link from 'next/link';
 import Image from 'next/image';
 import emptyImage from '../app/assets/empty.jpg';
 import exploresection from '../app/assets/home_images/explore-bg.png';
- 
 
-function ExplorationsFarAway({loading,city}) {
-
-  let result=city?city.result:[]
- 
-
-  
+function ExplorationsFarAway({ loading, city }) {
+  let result = city ? city.result : [];
 
   let reversedFilterCities = result ? [...result].reverse() : [];
 
   return (
-    <div className='explore-section' style={{ backgroundImage: `url(${exploresection.src})` }}>
+    <div
+      className="explore-section"
+      style={{ backgroundImage: `url(${exploresection.src})` }}
+    >
       <div className="explorations-container container inner-w-container">
-        <h2 className='same_heading'>Explore Best Cities</h2>
-        <div className='link_heading'>
+        <h2 className="same_heading">Explore Best Cities</h2>
+        <div className="link_heading">
           <p>Ideal for 5-14 days trip</p>
-          <Link href="/packages"><span className="view-all">View All Cities</span></Link>
+          <Link href="/packages">
+            <span className="view-all">View All Cities</span>
+          </Link>
         </div>
 
         <div className="destinations-grid">
-          {reversedFilterCities === undefined || reversedFilterCities===null ? (
+          {reversedFilterCities.length === 0 ? (
             <EmptyExplorationComponent />
           ) : (
             reversedFilterCities.slice(0, 6).map((destination) => (
               <Link href={`/city/${destination.slug}`} key={destination._id}>
-              <div  className="destination-card">
-                {destination.images && destination.images.length > 0 ? (
-                  destination.images.map((image) => (
+                <div className="destination-card">
+                  {destination.images && destination.images.length > 0 ? (
                     <Image
-                      key={image._id}
-                      src={`/uploads/${image.name}`}
-                      alt={destination.name || "loading..."}
+                      key={destination.images[0]._id}
+                      src={`/uploads/${destination.images[0].name}`}
+                      alt={destination.name || 'loading...'}
                       className="destination-image"
                       width={1000}
                       height={300}
+                      priority
                     />
-                  ))
-                ) : (
-                  <Image
-                    src={emptyImage.src}
-                    alt="empty_image"
-                    className="destination-image"
-                    width={1000}
-                    height={300}
-                  />
-                )}
-                <div className="destination-info">
-                  <h3>{destination.title}</h3>
-                  <p>From ₹ {destination.package ? destination.package.price : 'N/A'}</p>
+                  ) : (
+                    <Image
+                      src={emptyImage.src}
+                      alt="empty_image"
+                      className="destination-image"
+                      width={1000}
+                      height={300}
+                      priority
+                    />
+                  )}
+                  <div className="destination-info">
+                    <h3>{destination.title}</h3>
+                    <p>
+                      From ₹{' '}
+                      {destination.package ? destination.package.price : 'N/A'}
+                    </p>
+                  </div>
                 </div>
-              </div>
               </Link>
             ))
           )}
@@ -69,21 +71,24 @@ function ExplorationsFarAway({loading,city}) {
 function EmptyExplorationComponent() {
   return (
     <>
-      {Array(6).fill().map((_, index) => (
-        <div key={index} className="destination-card">
-          <Image
-            src={emptyImage.src}
-            alt="Loading"
-            className="destination-image"
-            width={1000}
-            height={300}
-          />
-          <div className="destination-info">
-            <h3>loading...</h3>
-            <p>loading...</p>
+      {Array(6)
+        .fill()
+        .map((_, index) => (
+          <div key={index} className="destination-card">
+            <Image
+              src={emptyImage.src}
+              alt="Loading"
+              className="destination-image"
+              width={1000}
+              height={300}
+              priority
+            />
+            <div className="destination-info">
+              <h3>loading...</h3>
+              <p>loading...</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </>
   );
 }
