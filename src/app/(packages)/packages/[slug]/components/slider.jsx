d5fe3158f6cdc1846a887b1757fe9048ce96275a
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-08-20 12:55:46 +0530

    changes

--- src/app/(packages)/packages/[slug]/components/slider.jsx
+++ src/app/(packages)/packages/[slug]/components/slider.jsx
@@ -1,20 +1,43 @@
-import Image from 'next/image';
+'use client'
 
 const TravelGallery = ({ result }) => {
-
   return (
-    <div className="gallery packages-innerpage">
+    <div className="gallery">
       <h2>Passionate Paris With Disney 4N-5D</h2>
       <div className="images">
-        {result === undefined || result === null ? ('no result found') : (result.map((ele, index) => {
-          return <div className="image-container" key={index}>
-            {ele.packages_galleries === null || ele.packages_galleries === undefined||ele.packages_galleries.length===0 ? ('no result found') : (ele.packages_galleries.slice(0,3).map((e, index) => {
-              return <img src={`/uploads/${e.name}`} alt="Image 1" key={index}/>
-
-            }))}
-          </div>
-        }))}
-       
+        {result === undefined || result === null ? (
+          'No result found'
+        ) : (
+          result.map((ele, index) => (
+            <div className="imageContainer" key={index}>
+              {ele.packages_galleries === null ||
+              ele.packages_galleries === undefined ||
+              ele.packages_galleries.length === 0 ? (
+                'No result found'
+              ) : (
+                <>
+                  <div className="leftImage">
+                    <img
+                      src={`/uploads/${ele.packages_galleries[0].name}`}
+                      alt="Large Image"
+                      className="largeImage"
+                    />
+                  </div>
+                  <div className="rightImages">
+                    {ele.packages_galleries.slice(1, 3).map((e, idx) => (
+                      <img
+                        src={`/uploads/${e.name}`}
+                        alt={`Image ${idx + 2}`}
+                        key={idx}
+                        className="smallImage"
+                      />
+                    ))}
+                  </div>
+                </>
+              )}
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
