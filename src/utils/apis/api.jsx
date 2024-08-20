5c55772107c8999f2cd422d0523e5cf7f6252758
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-08-20 18:03:00 +0530

    changes

--- src/utils/apis/api.jsx
+++ src/utils/apis/api.jsx
@@ -237,30 +237,33 @@ export const EXPORT_ALL_APIS = () => {
             return data;
         });
     };
-    const loadFooterDeatails = () => fetchApi(`/api/v1/footer-details/get`);
 
     const loadAllContinents = () => fetchApi(`/api/v1/continents/get`);
+    const loadSingleContinent = (slug) => fetchApi(`/api/v1/continent/getbyslug/${slug}`);
+    
     const loadAllCountries = () => fetchApi(`/api/v1/countries/get`);
+    const loadSingleCountry = (slug) => fetchApi(`/api/v1/countries/get/${slug}`);
+    
+    const loadSingleCity = (slug) => fetchApi(`/api/v1/city/getbyslug/${slug}`);
     const loadAllCities = () => fetchApi(`/api/v1/cities/get`);
     const loadAllCitiesWithLowestPrices = () => fetchApi(`/api/v1/cities/cities-with-lowest-price`);
-    const loadAllPackages = () => fetchApi(`/api/v1/packages/get`);
-    const loadAllActivities = () => fetchApi(`/api/v1/activities/get`);
-    const loadAllPackagesActivities = () => fetchApi(`/api/v1/package-categories/get`);
-    const loadAllBlogs = () => fetchApi(`/api/v1/blogs/get`);
     
-
-
-    const loadSingleContinent = (slug) => fetchApi(`/api/v1/continent/getbyslug/${slug}`);
-    const loadSingleCountry = (slug) => fetchApi(`/api/v1/countries/get/${slug}`);
-    const loadSingleCity = (slug) => fetchApi(`/api/v1/city/getbyslug/${slug}`);
+    const loadAllPackages = () => fetchApi(`/api/v1/packages/get`);
     const loadSinglePackage = (slug) => fetchApi(`/api/v1/package/getbyslug/${slug}`);
+    
+    const loadAllBlogs = () => fetchApi(`/api/v1/blogs/get`);
     const loadSingleBlog = (slug) => fetchApi(`/api/v1/blog/getbyslug/${slug}`);
+    
+    const loadAllActivities = () => fetchApi(`/api/v1/activities/get`);
     const loadSingleActivity = (slug) => fetchApi(`/api/v1/activity/getbyslug/${slug}`);
+    
+    const loadAllPackagesActivities = () => fetchApi(`/api/v1/package-categories/get`);
     const loadSinglePackagesActivity = (slug) => fetchApi(`/api/v1/package-category/getbyslug/${slug}`);
     const loadSinglePackagesActivitiy= async (slug) =>fetchApi(`/api/v1/package-category/getbyslug/${slug}`);
+
+    const loadFooterDeatails = () => fetchApi(`/api/v1/footer-details/get`);
+    
     const loadSingleUserbookingsdetails = (user_id) => fetchApi(`/api/v1/otpuser/getbyid/${user_id}`);
-
-
     
     const sendQueryContactUs = (formData) => fetchApi(`/api/v1/sendquery/query/send`, { method: 'POST', body: formData });
     const sendQueryBookings = (formData) => fetchApi(`/api/v1/booking/sendquery`, { method: 'POST', body: formData });
@@ -286,11 +289,11 @@ export const EXPORT_ALL_APIS = () => {
         loadAllCitiesWithLowestPrices,
         loadAllPackages,
         loadSinglePackage,
-        loadAllActivities,
-        loadAllPackagesActivities,
         loadAllBlogs,
         loadSingleBlog,
+        loadAllActivities,
         loadSingleActivity,
+        loadAllPackagesActivities,
         loadSinglePackagesActivity,
         loadSinglePackagesActivitiy,
         loadFooterDeatails,
